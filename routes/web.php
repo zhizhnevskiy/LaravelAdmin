0faa5bcd4aec65a58eb5eb8a247af869d83dd9b4
y.zhizhnevskiy <CN7FFmHS>
2022-10-12 12:56:27 +0300

    start developing and add free adminlte

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix'=>'admin'], function (){
+   Route::get('/', [MainController::class, 'index'])->name('admin.index');
 });
